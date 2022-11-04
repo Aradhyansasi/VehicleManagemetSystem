ab35128f59104baffdc5f07d1ccc5f048932e4c2
Aradhyansasi <aradhya.n.sasi101@gmail.com>
2022-11-04 17:50:29 +0530

    add validation

--- superAdmin/models.py
+++ superAdmin/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 # Create your models here.
 
 
 class Details(models.Model):
-    vehicle_number = models.CharField(max_length=50, unique=True)
+    alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+    alphabet = RegexValidator(r'^[a-zA-Z]*$', 'Only characters are allowed.')
+    vehicle_number = models.CharField(max_length=50, unique=True, validators=[alphanumeric])
 
     options = (
         ('Two Wheelers', 'Two Wheelers'),
@@ -14,8 +17,8 @@ class Details(models.Model):
     )
 
     vehicle_type = models.CharField(max_length=50,choices=options,default='Two Wheelers')
-    vehicle_model = models.CharField(max_length=50)
-    vehicle_description = models.CharField(max_length=100)
+    vehicle_model = models.CharField(max_length=50,validators=[alphabet])
+    vehicle_description = models.CharField(max_length=100,validators=[alphabet])
 
     def __str__(self):
         return self.vehicle_number
